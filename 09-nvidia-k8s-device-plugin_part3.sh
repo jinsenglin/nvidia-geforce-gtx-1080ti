226d3955cc1528774c520737219539eff9ef18c0
Jim Lin <jimlintw922@gmail.com>
2018-07-09 15:01:20 +0800

    add 09-nvidia-k8s-device-plugin_part3.sh

---
+++ 09-nvidia-k8s-device-plugin_part3.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+cat <<DATA | kubectl apply -f -
+apiVersion: v1
+kind: Pod
+metadata:
+  name: cuda-vector-add
+spec:
+  restartPolicy: OnFailure
+  containers:
+    - name: cuda-vector-add
+      image: "k8s.gcr.io/cuda-vector-add:v0.1"
+      resources:
+        limits:
+          nvidia.com/gpu: 1 # requesting 1 GPU
+DATA
