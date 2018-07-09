f40cf749b42a6ad4e5cce1b1b2d47ece8606c0e7
Jim Lin <jimlintw922@gmail.com>
2018-07-09 15:22:24 +0800

    mod 09-nvidia-k8s-device-plugin_part3.sh

--- 09-nvidia-k8s-device-plugin_part3.sh
+++ 09-nvidia-k8s-device-plugin_part3.sh
@@ -16,3 +16,20 @@ spec:
         limits:
           nvidia.com/gpu: 1 # requesting 1 GPU
 DATA
+
+kubectl get po
+
+cat <<DATA | kubectl delete -f -
+apiVersion: v1
+kind: Pod
+metadata:
+  name: cuda-vector-add
+spec:
+  restartPolicy: OnFailure
+  containers:
+    - name: cuda-vector-add
+      image: "k8s.gcr.io/cuda-vector-add:v0.1"
+      resources:
+        limits:
+          nvidia.com/gpu: 1 # requesting 1 GPU
+DATA
